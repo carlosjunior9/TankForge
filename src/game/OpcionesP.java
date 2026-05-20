52984e29d39de6b791df0c2e0df00fc4e9ee4bdb
carlosjunior9 <juniorbetancur715@gmail.com>
2026-05-20 17:19:39 -0400

    feat: Agregando pruebas

---
+++ src/game/OpcionesP.java
@@ -0,0 +1,12 @@
+package game;
+
+import javax.swing.JFrame;
+
+public class OpcionesP {
+    
+    public OpcionesP(){
+        JFrame ventana = new JFrame("Opciones");
+        ventana.setSize(500, 500);
+        ventana.setVisible(true);
+    }
+}
