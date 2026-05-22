f632bff003587e4214895526c53af932d6da2611
carlosjunior9 <juniorbetancur715@gmail.com>
2026-05-21 22:31:58 -0400

    feat: agrege las imagenes a los botones aun falta arreglar

--- src/game/PanelDelJuego.java
+++ src/game/PanelDelJuego.java
@@ -12,10 +12,42 @@ public class PanelDelJuego extends JPanel {
 
         setLayout(null);
 
-        JButton Iniciar = new JButton("INICIAR");
+        ImageIcon iniciar = new ImageIcon("TankForge/assets/fondos/Inicio.png");
+        
+        Image iniciarEscalada = iniciar.getImage().getScaledInstance(200, 50, Image.SCALE_SMOOTH);
+
+        ImageIcon iniciarImg = new ImageIcon(iniciarEscalada);
+
+
+
+        ImageIcon opciones = new ImageIcon("TankForge/assets/fondos/Opciones.png");
+
+        Image opcionesEscalada = opciones.getImage().getScaledInstance(200, 50, Image.SCALE_SMOOTH);
+        
+        ImageIcon opcionesImg = new ImageIcon(opcionesEscalada);
+
+
+
+    ImageIcon salirOriginal =
+                new ImageIcon("TankForge/assets/fondos/Salir.png");
+
+        Image salirEscalada =
+                salirOriginal.getImage().getScaledInstance(
+                        250,
+                        70,
+                        Image.SCALE_SMOOTH
+                );
+
+        ImageIcon salirImg =
+                new ImageIcon(salirEscalada);
+        
+
+
+        JButton Iniciar = new JButton(iniciarImg);
         Iniciar.setBounds(510, 350, 200, 50);
-        Iniciar.setForeground(Color.white);
-        Iniciar.setBackground(new Color(90, 100, 110));
+        Iniciar.setBorderPainted(false);
+        Iniciar.setContentAreaFilled(false);
+        Iniciar.setFocusPainted(false);
         Iniciar.addActionListener(e -> {
             JFrame Juego = (JFrame) SwingUtilities.getWindowAncestor(this);
             Juego.setContentPane(new PantallaJuego());
@@ -24,16 +56,18 @@ public class PanelDelJuego extends JPanel {
 
         add(Iniciar);
 
-        JButton Opciones = new JButton("OPCIONES");
+        JButton Opciones = new JButton(opcionesImg);
         Opciones.setBounds(510, 450, 200, 50);
-        Opciones.setForeground(Color.white);
-        Opciones.setBackground(new Color(90, 100, 110));
+        Iniciar.setBorderPainted(false);
+        Iniciar.setContentAreaFilled(false);
+        Iniciar.setFocusPainted(false);
         add(Opciones);
 
-        JButton Salir = new JButton("SALIR");
+        JButton Salir = new JButton(salirImg);
         Salir.setBounds(510, 550, 200, 50);
-        Salir.setForeground(Color.white);
-        Salir.setBackground(new Color(90, 100, 110));
+        Iniciar.setBorderPainted(false);
+        Iniciar.setContentAreaFilled(false);
+        Iniciar.setFocusPainted(false);
 
         Salir.addActionListener(e -> {
             System.exit(0);
